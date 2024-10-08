449172ad014404191842c920bea5018730ec4e5a
Andrei Makar-Uvarov <lyykfi@gmail.com>
2024-10-08 03:25:50 +0300

    wip

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,24 @@
 import './bootstrap';
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { createRouter, createWebHistory } from 'vue-router'
+import App from './App.vue'
+import Users from './Pages/Users.vue'
+
+
+const routes = [ 
+    { path: '/', component: Users },
+] 
+ 
+const router = createRouter({ 
+    history: createWebHistory(),
+    routes
+}) 
+
+const app = createApp(App)
+
+app.use(createPinia())
+// app.use(router)
+
+app.mount('#app')
